06851dc8d6439e12738308a67ab04585a4d0c708
Neacail Grech <neacail.grech@alludo.com>
2025-04-29 10:57:42 +0200

    Updated CVAD Export

--- Tools/Migration Tool for Citrix Virtual Apps and Desktops/CVAD7xExport.ps1
+++ Tools/Migration Tool for Citrix Virtual Apps and Desktops/CVAD7xExport.ps1
@@ -9,36 +9,51 @@
 
 .PARAMETERS folder
     The empty folder to place the exported files in. If not existent will be created.
+    
+.PARAMETERS cloud
+    Work with Citrix DaaS rather than on-prem
+
+.PARAMETERS customerId
+    The customer id for Citrix DaaS
+
+.PARAMETERS profileName
+    The local Citrix DaaS profile name to use for authentication. Previously saved with Set-XDCredentials
 
 .PARAMETERS overwrite
     When specified will not exit if the destination folder already contains files
 
 .PARAMETERS maxRecordCount
     The maximum number of records to retrieve from Citrix
+    
+.PARAMETERS limitAppGroups
+    Only export as many as this number of app groups. Designed to allow quick testing.
+    
+.PARAMETERS limitApps
+    Only export as many as this number of apps. Designed to allow quick testing.
 
 .EXAMPLE
     & '.\CVAD7xExport.ps1' -ddc xaddc01 -folder c:\temp\CitrixExport
 
     Connect to the delivery controller and export the configuration to the given folder
+    
+.EXAMPLE
+    & '.\CVAD7xExport.ps1' -cloud -folder c:\temp\CitrixExport
+
+    Connect to Citrix DaaS with interactive (browser) authentication and export the configuration to the given folder
 
 .NOTES
-    Requires Citrix CVAD PowerShell cmdlets to be installed (not the Remote SDK)
-
-Modification History:
-
-    2022/12/21  @guyrleech  First official release
-    2023/01/18  @guyrleech  Make CommandLineExecutable contain executable and parameters
+    Requires Citrix CVAD PowerShell cmdlets to be installed or the the Remote SDK when using DaaS
 #>
 
 <#
-Copyright � 2022 Guy Leech
+Copyright © 2024 Guy Leech
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the �Software�), to deal in the Software without restriction, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, 
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED �AS IS�, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
 
@@ -46,18 +61,30 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMA
 
 Param
 (
+    [Parameter(ParameterSetName='Cloud',Mandatory=$false)]
+    [switch]$cloud ,
+    [Parameter(ParameterSetName='OnPrem',Mandatory=$true)]
     [string]$ddc ,
     [Parameter(Mandatory=$true,HelpMessage='Base folder to write exported CLIXML files')]
     [string]$folder ,
+    [Parameter(ParameterSetName='Cloud',Mandatory=$false)]
+    [string]$customerId ,
+    [Parameter(ParameterSetName='Cloud',Mandatory=$false)]
+    [string]$profileName ,
     [int]$maxRecordCount = 50000 ,
+    [int]$limitAppGroups ,
+    [int]$limitApps ,
     [switch]$overwrite
     ## TODO implement parameters for filtering on specific types or names matching pattern/regex
 )
 
 ## map Citrix properties to RAS ones - empty string means remove the property, not in the table means pass through
 ## + at start of value means replace the :: delimited property names with the value for those properties. If property does not exist or is empty, the replacement will be the empty string
+## @ at start means the data in the source will be morphed as per a separate mapping entry
 [hashtable]$CitrixApplicationsToRAS = @{
     'CommandLineExecutable' = '+"::CommandLineExecutable::" ::CommandLineArguments::'
+    'AssociatedUserNames' = '@Accounts : ^(?<domain>.+)\\(?<user>.+)$' ## Each source item will be parsed to match the regex and each instance of the named match etc will be replaced by the corresponding $match
+    'AssociatedUserSIDs' = '' ## no guarantee that order of this array is same as user names so discard SIDs and look them up afresh when mapping AssociatedUserNames
     'EncodedIconData' = 'IconData'
     'PublishedName' = 'DisplayName'
     'AllAssociatedDesktopGroupUids' = ''
@@ -73,6 +100,21 @@ Param
     'Uid' = ''
     'IconUid' = ''
     'AdminFolderUid' = ''
+}
+
+## mappings for @items
+## strings will be "executed" after placeholders filled in so functions, etc can be called on them and return value is what goes in output. %var% replaced by $match of that named group
+[hashtable]$structureMappings = @{
+    'Accounts' = @{
+        AccountDisplayName = '%domain%\%user%'
+        AccountName = '%user%'
+        AccountType = '$(Get-AccountInfo `"%user%`" ''AccountType'' )' ## don't use domain which may cause problems in multi-domain/trusts - TODO cater for domain (how do we get FQDN from just domain name?)
+        AccountId = '0X2/NT/%domain%/$((New-Object System.Security.Principal.NTAccount(`"%domain%\%user%`")).Translate([System.Security.Principal.SecurityIdentifier]).value)'
+        AccountAuthority = 'NTDomain/%domain%'
+        OtherName = '%user%'
+        SearchPath = '$(Get-AccountInfo `"%user%`" ''OU'' )'
+        MachineName = $env:COMPUTERNAME
+    }
 }
 
 [hashtable]$CitrixDeliveryGroupsToRAS = @{
@@ -92,6 +134,51 @@ Param
 [hashtable]$CitriSitesToRAS = @{
     'Name' = 'FarmName'
     'LicensedSessionsActive' = 'SessionCount'
+}
+
+[System.Collections.Generic.List[string]]$citrixModules = @( 'Citrix.Broker.Admin.V2' , 'Citrix.Configuration.Admin.V2' )
+
+Function Get-AccountInfo
+{
+    Param
+    (
+        [string]$accountName ,
+        [ValidateSet('AccountType','OU')]
+        [string]$attribute = 'AccountType'
+    )
+    [string]$result = ''
+    Write-Verbose -Message "Get-AccountInfo( $accountName , $attribute )"
+    $searcher = [ADSISearcher]"(&(sAMAccountName=$accountName))"
+    $account = $null
+    $account = $searcher.FindOne()
+    if( $null -ne $account )
+    {
+        if( $attribute -eq 'AccountType' )
+        {
+            if( $account.Properties.objectclass -icontains 'group' )
+            {
+                $result = 'Group'
+            }
+            elseif( $account.Properties.objectclass -icontains 'person' -or $account.Properties.objectclass -icontains 'user' )
+            {
+                $result = 'User'
+            }
+            else
+            {
+                Write-Warning -Message "Account $accountName of unexpected type $($account.Properties.objectclass -join '#' )"
+            }
+        }
+        elseif( $attribute -eq 'OU' )
+        {
+            ## remove first part of string which is name of object itself CN=Citrix_PublishedApp_PowerShell,OU=Groups,OU=Users,OU=Wakefield,OU=Sites,DC=guyrleech,DC=local
+            $result = $account.Properties.distinguishedname -replace '^[^,]+,'
+        }
+        $result ## output
+    }
+    else
+    {
+        Write-Warning -Message "Failed to find account $accountName"
+    }
 }
 
 Function Convert-CitrixToRAS
@@ -136,7 +223,7 @@ Function Convert-CitrixToRAS
                     }
                     elseif( $propertyMapping.Length -gt 0 )
                     {
-                        if( $propertyMapping[0] -eq '+' ) ## we have some moprhing to do
+                        if( $propertyMapping[0] -eq '+' ) ## we have some morphing to do
                         {
                             [string]$expandedValue = $propertyMapping.SubString( 1 )
                             ## replace each : delimited property with its property value, if present
@@ -170,6 +257,56 @@ Function Convert-CitrixToRAS
                             }
                             $result.Add( $property.Name , $expandedValue.Trim() )
                         }
+                        elseif( $propertyMapping[0] -eq '@' ) ## we have to create a new property and populate via placeholders
+                        {
+                            ## @Accounts : ^(?<domain>.+)\\(?<user>.+)$
+                            if( $propertyMapping -notmatch '^@\s*(\w+)\s*:\s*(.+)$' )
+                            {
+                                Write-Warning -Message "Unparsable format in `"$propertyMapping`""
+                            }
+                            else
+                            {
+                                [string]$propertyName = $matches[ 1 ]
+                                [string]$regex = $matches[ 2 ]
+                            
+                                $structureMapping = $structureMappings[ $propertyName ]
+
+                                if( $null -eq $structureMapping )
+                                {
+                                    Write-Warning -Message "No mapping structure found for property name `"$propertyName`" in string $propertyName"
+                                }
+                                else
+                                {
+                                    [array]$mapped = @(ForEach( $value in $property.Value )
+                                    {
+                                        [hashtable]$newValues = @{}
+                                        if( $value -match $regex )
+                                        {
+                                            ForEach( $mappedItem in $structureMapping.GetEnumerator() )
+                                            {
+                                                [string]$resultString = $mappedItem.Value
+                                                ForEach( $match in $matches.GetEnumerator() )
+                                                {
+                                                    $resultString = $resultString -replace "%$($match.Name)%" , $match.Value
+                                                }
+                                                Write-Verbose -Message "Morphed property `"$($mappedItem.Name)`", value `"$($mappedItem.Value)`" to `"$resultString`""
+                                                ## TODO now "execute" string to expand any functions,cmdlets, etc (<-- attack vector!!)
+                                                $scriptBlock = [System.Management.Automation.ScriptBlock]::Create( "`"$resultString`"" )
+                                                $resultString = $scriptBlock.Invoke()
+                                                Write-Verbose -Message "String now $resultString"
+                                                $newValues.Add( $mappedItem.Name , $resultString )
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Write-Warning -Message "Property value `"$value`" in property `"$($property.name)`" does not match regex $regex"
+                                        }
+                                        [pscustomobject]$newValues ## output
+                                    })
+                                    $result.Add( $propertyName , $mapped )
+                                }
+                            }
+                        }
                         else ## simple string replacement
                         {
                             $result.Add( $propertyMapping , $property.Value )
@@ -198,12 +335,24 @@ elseif( -Not (New-Item -Path $folder -ItemType Directory -Force))
 {
     Throw "Failed to created folder `"$folder`""
 }
+if( -Not [string]::IsNullOrEmpty( $profileName ) -or -Not [string]::IsNullOrEmpty( $customerId )  )
+{
+    $cloud = $true
+    $citrixModules.Add( 'Citrix.Sdk.Proxy.V*' )
+}
 
 ## TODO Try ipmo
-if( ! ( Get-PSSnapin Citrix.Broker.Admin.V2 -EA SilentlyContinue ) )
+ForEach( $snapin in $citrixModules )
 {
-    Add-PSSnapin Citrix.Broker.Admin.V2
+    if( -Not ( Get-PSSnapin -Name $snapin -ErrorAction SilentlyContinue ) )
+    {
+        Add-PSSnapin -Name $snapin
+    }
 }
+
+## get AD so we can find accounts later if we need to
+$rootDSE = [ADSI]"LDAP://RootDSE"
+$domainDN = $rootDSE.Get("defaultNamingContext")
 
 $outputObject = $null
 $inputObject = $null
@@ -214,6 +363,30 @@ $inputObject = $null
 if( -Not [string]::IsNullOrEmpty( $ddc ) -and $ddc -ine 'localhost' -and $ddc -ne '.' )
 {
     $citrixCommonParameters.Add( 'AdminAddress' , $ddc )
+}
+elseif( $cloud -or -not [string]::IsNullOrEmpty( $profileName ) -or -Not [string]::IsNullOrEmpty( $customerId ) )
+{
+    if( -Not ( Get-Command -Name Get-XDAuthentication -ErrorAction SilentlyContinue ))
+    {
+        Throw "Unable to attempt to authenticate to Citrix Cloud - is the Remote PowerShell SDK installed (https://download.apps.cloud.com/CitrixPoshSdk.exe) ?"
+    }
+
+    [hashtable]$authParameters = @{ ErrorVariable = 'cloudAuthError' }
+    if( -Not [string]::IsNullOrEmpty( $profileName ) )
+    {
+        $authParameters.Add( 'ProfileName' , $profileName ) 
+    }
+    if( -Not [string]::IsNullOrEmpty( $customerId ) )
+    {
+        $authParameters.Add( 'CustomerId' , $customerId ) 
+    }
+    $cloudAuthError = $null
+    $auth = Get-XDAuthentication @authParameters
+    $cloud = $?
+    if( -Not $cloud )
+    {
+        Throw "Authentication to Citrix Cloud failed: $cloudAuthError"
+    }
 }
 
 [hashtable]$citrixDDCParameter = $citrixCommonParameters.Clone() ## used when Get-Broker* cmdlet doesn't take -MaxRecordCount
@@ -227,7 +400,8 @@ $outputObject = [pscustomobject]@{
     'DeliveryController' = $ddc
     'Site' = @( Get-BrokerSite @citrixDDCParameter ) ## not used for restore, just for info
     'Zones' = @( Get-ConfigZone @citrixCommonParameters )
-    'Controllers' = @( Get-BrokerController @citrixCommonParameters ) ## not used for restore, just for info
+    ## Get-BrokerController fails for cloud so put customer id in instead
+    'Controllers' = $(if( $cloud ) { @( [pscustomobject]@{ 'Customer id' = $customerId } ) } else { @( Get-BrokerController @citrixCommonParameters ) } ) ## not used for restore, just for info
     #'ProvisioningSchemes' = @( Get-ProvScheme @citrixCommonParameters )
     #'HypervisorConnections' = @( Get-BrokerHypervisorConnection @citrixCommonParameters ) ## we don't create these as need credentials but need to ensure they exist
     #'IdentityPools' = @( Get-AcctIdentityPool @citrixCommonParameters )
@@ -242,9 +416,17 @@ $outputObject = [pscustomobject]@{
 Add-Member -InputObject $outputObject -MemberType NoteProperty -Name Policies -Value (Export-BrokerDesktopPolicy)
     
 [array]$appGroups = @( Get-BrokerApplicationGroup @citrixCommonParameters )
+[int]$counter = 0
 
 ForEach( $appGroup in $appGroups )
 {
+    $counter++
+    Write-Verbose -Message "$counter / $($appGroups.Count): appgroup $($appGroup.Name)"
+    if( $limitAppGroups -gt 0 -and $counter -gt $limitAppGroups )
+    {
+        Write-Warning -Message "Hit limit of $limitAppGroups on $($appGroups.Count) app groups items total"
+        break
+    }
     ## Get Desktop group so we can export
     ## Store delivery group names rather than GUIDs as they may change when we create new
     [string]$deliveryGroups = $null
@@ -269,13 +451,26 @@ Add-Member -InputObject $outputObject -MemberType NoteProperty -Name Application
     
 [array]$apps = @( Get-BrokerApplication @citrixCommonParameters )
 
-## Write-Verbose "Backing up $($apps.Count) apps to `"$folder`""
+Write-Verbose "Backing up $($apps.Count) apps to `"$folder`""
+$counter = 0
+
 ForEach( $app in $apps )
 {
+    $counter++
+    Write-Verbose -Message "$counter / $($apps.Count): app $($app.Name)"
+    if( $limitApps -gt 0 -and $counter -gt $limitApps )
+    {
+        Write-Warning -Message "Hit limit of $limitApps on $($apps.Count) apps items total"
+        break
+    }
     ## Get Desktop group so we can export
     ## If more than one, we'll make a delimited list so can store in this object AssociatedDesktopGroupUids 
     #[string]$deliveryGroups = $null
-            
+    ## trailing \ can break import
+    $app.AdminFolderName = $app.AdminFolderName -replace '\\+$'
+    $app.ClientFolder = $app.ClientFolder -replace '\\+$'
+    $app.StartMenuFolder = $app.StartMenuFolder -replace '\\+$'
+
     [array]$associatedDeliveryGroups = @( $app.AssociatedDesktopGroupUids | ForEach-Object `
     {
         $AssociatedDesktopGroupUid = $_
@@ -307,8 +502,8 @@ ForEach( $app in $apps )
     ## we'll add the apps to the app group export later too
     Add-Member -InputObject $app -MemberType NoteProperty -Name AssociatedAppGroups -Value $AssociatedApplicationGroups
 
-    ## now dump icon to file
-    if( ! $app.IconFromClient )
+    ## dump icon to file
+    if( -Not $app.IconFromClient )
     {
         Add-Member -InputObject $app -MemberType NoteProperty -Name EncodedIconData -Value (Get-BrokerIcon -Uid $app.IconUid).EncodedIconData
     }
